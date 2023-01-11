241f6c86759632368ff52eec3252d393eb94b56e
lexrama <alexa.ramachandran@gmail.com>
2023-01-11 11:42:17 -0500

    Update .gitignore

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hewwo Edit <code>src/App.js</code> and save to reload.
+          slayyy
         </p>
         <a
           className="App-link"
