c6cf266e60eb69129b93553c9fbb11c42ac304f3
Alexa Ramachandran <alexa.ramachandran@gmail.com>
2023-01-27 14:37:35 -0500

    Start fresh with new header, add HeaderSocials.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       yuh
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <div>
+    <div className="App">
       <Header />
+      {/* <header className="App-header">
+        <p>
+          yuh
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn wot
+        </a>
+      </header> */}
     </div>
+    // <div>
+    //   <Header />
+    // </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,88 +1,34 @@
-import React, { useState } from "react";
-import "./Header.css"; 
-import { NavLink, Link } from "react-router-dom";
+// import React, { useState } from "react";
+import "./Header.css";
+import HeaderSocials from "./HeaderSocials";
+// import { NavLink, Link } from "react-router-dom";
 
 
 const Header = () => {
-  return (
-	<div>
-	<header className="header" style={{backgroundColor: 'black'}}>
-		{/* <NavLink to={link} tag={Link} className="logo">
-		<span style={{ color: theme.text }}></span>
-		<span className="logo-name" style={{ color: theme.text }}>
-			{greeting.logo_name}
-		</span>
-		<span style={{ color: theme.text }}></span>
-		</NavLink> */}
-		{/* <input className="menu-btn" type="checkbox" id="menu-btn" />
-		<label className="menu-icon" htmlFor="menu-btn">
-		<span className="navicon"></span>
-		</label> */}
-		<ul className="menu">
-		<li>
-			{/* <NavLink
-			className="homei"
-			to="/home"
-			tag={Link}
-			activeStyle={{ fontWeight: "bold" }}
-			style={{ borderRadius: 5, color: theme.text }}
-			>
-			Home
-			</NavLink> */}
-			<span className="homei">Home</span>
-		</li>
-		<li>
-			{/* <NavLink
-			// className="ec"
-			// to="/education"
-			// tag={Link}
-			// activeStyle={{ fontWeight: "bold" }}
-			// style={{ borderRadius: 5 }}
-			>
-			Education and Certifications
-			</NavLink> */}
-			<span className="ec">Education and Certifications</span>
-		</li>
-		<li>
-			{/* <NavLink
-			className="xp"
-			to="/experience"
-			tag={Link}
-			activeStyle={{ fontWeight: "bold" }}
-			style={{ borderRadius: 5, color: theme.text }}
-			> */}
-			<span className="xp">Experience</span>
-			{/* </NavLink> */}
-		</li>
-		<li>
-			{/* <NavLink
-			className="projects"
-			to="/projects"
-			tag={Link}
-			activeStyle={{ fontWeight: "bold" }}
-			style={{ borderRadius: 5, color: theme.text }}
-			> */}
-			<span className="projects">Projects</span>
-			{/* </NavLink> */}
-		</li>
-		<li>
-			{/* <NavLink
-			className="cr"
-			to="/contact"
-			tag={Link}
-			activeStyle={{ fontWeight: "bold" }}
-			style={{ borderRadius: 5, color: theme.text }}
-			> */}
-			<span className="cr">Contact and Resume</span>
-			{/* </NavLink> */}
-		</li>
-		{/* <button {...styles} onClick={changeTheme}>
-			{icon}
-		</button> */}
-		</ul>
-	</header>
-	</div>
-  )
+	return (
+		<div>
+			<header className="header">
+				<ul>
+					<li>
+						<span className="homei">Home</span>
+					</li>
+					<li>
+						<span className="ec">Education</span>
+					</li>
+					<li>
+						<span className="xp">Experience</span>
+					</li>
+					<li>
+						<span className="projects">Projects</span>
+					</li>
+					<li>
+						<span className="cr">Resume</span>
+					</li>
+				</ul>
+				<HeaderSocials />
+			</header>
+		</div>
+	)
 }
 
 export default Header
