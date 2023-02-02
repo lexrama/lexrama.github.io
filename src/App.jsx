3adb1752f51ddd0a3a72cc087589364a4b3c8671
Alexa Ramachandran <alexa.ramachandran@gmail.com>
2023-02-01 21:43:38 -0500

    Rename extensions from .js to .jsx

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Projects from './pages/projects/Projects';
+import Experience from './pages/experience/Experience';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="experience" element={<Experience />} />
+            <Route path="projects" element={<Projects />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai"
+
+function Footer() {
+	return (
+		<Navbar bg="light" expand="lg">
+			<Container>
+				{/* <Navbar.Brand href="#home">Alexa Ramachandran</Navbar.Brand> */}
+				{/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						{/* <Nav.Link href="#home">About</Nav.Link> */}
+						<Nav.Link href='https://linkedin.com/in/lex-rama' className='socials' target='blank'><AiFillLinkedin /></Nav.Link>
+						<Nav.Link href='https://github.com/lexrama' className='socials' target='blank'><AiFillGithub /></Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
+
+export default Footer
