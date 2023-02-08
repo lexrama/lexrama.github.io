7d416502efde71dcbb3c6a96da0297d1da34e282
Alexa Ramachandran <alexa.ramachandran@gmail.com>
2023-02-08 17:48:56 -0500

    Add pictures to Experience

--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+import Microsoft from "../../assets/msft.png"
+import './Experience.css'
 
 const Experience = () => {
   return (
-	<div>Experience</div>
+
+    <>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="2011 - present"
+          iconStyle={{ size: '40px', background: 'rgb(33, 150, 243)', color: '#fff' }}
+        icon={<img className="test" src={Microsoft} alt="msft"/>}
+        >
+          <h3 className="vertical-timeline-element-title">Software Engineering Intern</h3>
+          <h4 className="vertical-timeline-element-subtitle">Microsoft</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </>
   )
 }
 
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import ProjectCard from "../../components/project-card/ProjectCard"
 import { Container, Col, Row } from "react-bootstrap"
-import Artally from "../../assets/artally.png"
+// import Artally from "../../assets/artally.png"
 
 const Projects = () => {
   return (
