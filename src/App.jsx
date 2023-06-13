c39385fdcd4c8853a3f6188ae98e98cf8b8e02f7
Alexa Ramachandran <alexa@blockpartyapp.com>
2023-06-13 11:47:26 -0400

    Clean up extra files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/home/Home';
-import About from './pages/about/About';
-import Projects from './pages/projects/Projects';
-import Experience from './pages/experience/Experience';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Projects from "./pages/projects/Projects";
+import Experience from "./pages/experience/Experience";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -17,17 +17,15 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="experience" element={<Experience />} />
-            <Route path="projects" element={<Projects />} />
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="experience" element={<Experience />} />
+          <Route path="projects" element={<Projects />} />
         </Routes>
         <Footer />
       </BrowserRouter>
     </>
   );
 }
-
-
 
 export default App;
