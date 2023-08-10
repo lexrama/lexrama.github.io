dd6e23453bf41420a78e29c4dddc98981ce77f46
Alexa Ramachandran <alexa@blockpartyapp.com>
2023-08-10 12:44:11 -0400

    remove timeline

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,12 +8,11 @@ const About = () => {
         data-flickr-embed="true"
         href="https://www.flickr.com/gp/133825690@N04/o1V00164Tp"
         title="Best Shots"
+        target="_blank"
+        rel="noreferrer"
       >
-        best shots
         <img
           src="https://live.staticflickr.com/7675/26497677404_93a53597bf_z.jpg"
-          width="640"
-          height="480"
           alt="Best Shots"
         />
       </a>
@@ -21,7 +20,7 @@ const About = () => {
         async
         src="//embedr.flickr.com/assets/client-code.js"
         charset="utf-8"
-      ></script>
+      />
     </>
   );
 };
--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -1,32 +1,17 @@
-import React from 'react'
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import React from "react";
 
-import Microsoft from "../../assets/msft.png"
-import './Experience.css'
+import Microsoft from "../../assets/msft.png";
+import "./Experience.css";
 
 const Experience = () => {
   return (
-
     <>
-      <VerticalTimeline>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-          date="2011 - present"
-          iconStyle={{ size: '40px', background: 'rgb(33, 150, 243)', color: '#fff' }}
-        icon={<img className="test" src={Microsoft} alt="msft"/>}
-        >
-          <h3 className="vertical-timeline-element-title">Software Engineering Intern</h3>
-          <h4 className="vertical-timeline-element-subtitle">Microsoft</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-          </p>
-        </VerticalTimelineElement>
-      </VerticalTimeline>
+      <div className="experience">
+        <h1>Experience</h1>
+        <img src={Microsoft} alt="Microsoft" />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
