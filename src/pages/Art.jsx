9c0b7956591cb9f3329f016af7763fffb8057f44
Alexa Ramachandran <alexa.ramachandran@gmail.com>
2023-09-19 21:41:03 -0400

    Use consts for colors

--- src/pages/Art.jsx
+++ src/pages/Art.jsx
@@ -6,6 +6,7 @@ export default function Art() {
   return (
     <div className="Art">
       <h1>Photography 📸</h1>
+      <p>click below for a portfolio of some of my best shots :)</p>
       <FlickrCard />
     </div>
   );
